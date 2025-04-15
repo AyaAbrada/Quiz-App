1f25632e6b0958bb2f27394f096695ebf05cd756
AyaAbrada <abradaaya2@gmail.com>
2025-04-15 12:01:37 +0200

    Navbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { HistoriqueComponent } from './historique/historique.component';
+import { ParametreComponent } from './parametre/parametre.component';
+import { AccueilComponent } from './accueil/accueil.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'accueil', component: AccueilComponent },
+  { path: 'historique', component: HistoriqueComponent },
+  { path: 'parametre', component: ParametreComponent },
+  { path: '**', component: AccueilComponent, pathMatch: 'full'},
+];
+
