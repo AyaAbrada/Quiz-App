448e85562fb4b31cf56244778fa7730b84e7e041
AyaAbrada <abradaaya2@gmail.com>
2025-04-17 17:54:25 +0200

    la séléction des catégorie

--- src/app/accueil/accueil.component.ts
+++ src/app/accueil/accueil.component.ts
@@ -1,11 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, inject,  OnInit } from '@angular/core';
+
+import { CommonModule } from '@angular/common';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-accueil',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule , HttpClientModule],
   templateUrl: './accueil.component.html',
   styleUrl: './accueil.component.css'
 })
-export class AccueilComponent {
+export class AccueilComponent implements OnInit {
+ 
+  question : string = '' ;
+  private http = inject(HttpClient);
 
+  categories: Array<any> = [];
+  
+  selectedCategory: number = 9; 
+  selectedDifficulty: string = 'medium';
+  numQuestions: number = 10;
+  httpClient: any;
+  quizData: any;
+
+  ngOnInit(): void {
+    this.fetchCategories();
+  }
+
+  fetchCategories(): void {
+    this.http.get('https://opentdb.com/api_category.php')
+     .subscribe({
+      next: (res: any) => {
+        this.categories = res.trivia_categories;
+        console.log('Categories:', res);
+      },
+      error: (err) => {
+        console.error('Error fetching categories:', err);
+      }
+    });
+  }
 }
